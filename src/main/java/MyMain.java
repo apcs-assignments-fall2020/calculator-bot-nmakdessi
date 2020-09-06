ebf627eb6ca3f405b55a734412d28fab938f8bd6
nmakdessi <21nmakdessi@athenian.org>
2020-09-06 01:31:06 +0000

    Finished the assignment

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,30 +1,83 @@
+import java.net.SocketTimeoutException;
 import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
 
 public class MyMain {
 
     // Calculates the median of the three inputs
     public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+        if(a > b && b > c){
+            return b;
+        }
+        else if(b>a && a>c){
+            return a;
+        }
+        else if(a>c && c>b){
+            return c;
+        }
+        else if(a < b && b < c){
+            return b;
+        }
+        else if(b < a && a < c){
+            return a;
+        }
+        else if(a < c && c < b){
+            return c;
+        }
+        else{
+            return a;
+        }
+// not sure what to do if they are equal numbers
     }
 
     // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
+        if (Math.abs(a) > Math.abs(b) ) { 
+            return a; 
+        }
+        else {
+            return b; 
+        }
     }
 
-    // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
-    // where "a" and "b" are the inputs to the method
+    
     public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
+        return Math.sqrt((a*a) + (b*b));
     }
 
     public static void main(String[] args) {
-        // You may want to keep these lines of code to test that your methods work
-        System.out.println(median(1, 2, 3)); // should be 2
-        System.out.println(magnitude(-7, -1)); // should be -7
-        System.out.println(pythagoras(3, 4)); // should be 5.0
-
+        
         Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
+        System.out.println("which program would you like to run?");
+        String method = scan.nextLine();
+        if (method.equalsIgnoreCase("median")){
+            System.out.println("what is the first number?");
+            int a = scan.nextInt();
+            System.out.println("what is the second number?");
+            int b = scan.nextInt();
+            System.out.println("what is the third number?");
+            int c = scan.nextInt();
+            System.out.println("The median is " + median(a, b, c));
+        }
+        else if (method.equalsIgnoreCase("magnitude")){
+            System.out.println("What is the first number?");
+            int a = scan.nextInt();
+            System.out.println("What is the second number?");
+            int b = scan.nextInt();
+            System.out.println("The larger absolute value is " + magnitude(a, b));
+        }
+        else if (method.equalsIgnoreCase("pythagoras")){
+            System.out.println("What is the first number?");
+            int a = scan.nextInt();
+            System.out.println("What is the second number?");
+            int b = scan.nextInt();
+            System.out.println("The larger absolute value is " + pythagoras(a, b));
+        }
+        else{
+            System.out.println("Command not recognized");
+        }
+        scan.close();
     }
+
 }
